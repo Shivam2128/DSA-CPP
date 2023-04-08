b21d4cc80cd09ff259227d3d3758af9346214d91
Shivam Kumar Biswas <122352495+Shivam2128@users.noreply.github.com>
2023-04-08 20:52:54 +0530

    Create if else.cpp

---
+++ basic/loopcode/if else.cpp
@@ -0,0 +1,13 @@
+string compareNM(int n, int m){
+        // code here
+        if(n<m){
+            cout<< "lesser";
+        }
+        else if(n==m){
+            cout<< "equal";
+        }
+        else if(n>m){
+            cout<< "greater";
+        }
+    }
+};
