087bd2cfc8f2401db6f27c213ed0b2b93400d670
Shivam Kumar Biswas <122352495+Shivam2128@users.noreply.github.com>
2023-05-02 18:41:29 +0530

    pattren14.cpp

---
+++ basic/pattern/pattren14.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        int spaces = 2*(n-1);
+      
+      // Outer loop for the number of rows.
+      for(int i=1;i<=n;i++){
+          
+          // for printing numbers in each row
+          for(int j=1;j<=i;j++){
+              cout<<j;
+          }
+          
+          // for printing spaces in each row
+          for(int j = 1;j<=spaces;j++){
+              cout<<" ";
+          }
+          
+          // for printing numbers in each row
+          for(int j=i;j>=1;j--){
+              cout<<j;
+          }
+          
+          // As soon as the numbers for each iteration are printed, we move to the
+          // next row and give a line break otherwise all numbers
+          // would get printed in 1 line.
+          cout<<endl;
+          
+          // After each iteration nos. increase by 2, thus
+          // spaces will decrement by 2.
+          spaces-=2;
+      }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
