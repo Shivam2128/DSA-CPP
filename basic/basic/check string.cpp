2f3058a2e653902e8766b11f563eff8b5b61898e
Shivam Kumar Biswas <122352495+Shivam2128@users.noreply.github.com>
2023-05-09 10:15:18 +0530

    Create check string.cpp

---
+++ basic/basic/check string.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        bool check (string s)
+        {
+        	//code here.
+        	int len = s.size(); 
+        	   for(int i =1;  i< len-1; i++)
+        	   {
+        	       if(s[i] != s[0])
+        	        return false; 
+        	   }
+        	   return true;
+        }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+		string s;
+		cin >> s;
+		Solution ob;
+		if (ob.check (s))
+			cout << "YES\n";
+		else 
+			cout << "NO\n";
+	}
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
